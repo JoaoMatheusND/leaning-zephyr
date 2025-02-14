5154d23e2b29553c8a68f7ae7e01a155114085aa
JoaoMatheusND <jmnd@ic.ufal.br>
2025-02-14 16:03:31 -0300

    teste

--- src/main.c
+++ src/main.c
@@ -3,11 +3,15 @@
 #include <zephyr/drivers/uart.h>
 #include <stdio.h>
 #include <string.h>
+#define MSG_LEN 128
 
 /* Nome do dispositivo UART */
 #define UART_DEVICE_NODE DT_CHOSEN(zephyr_shell_uart)
 
 static const struct device *const uart_dev = DEVICE_DT_GET(UART_DEVICE_NODE);
+
+static char tx_buff[MSG_LEN];
+static int  tx_buff_pos;
 
 /* Configuração UART */
 struct uart_config uart_cfg = {
@@ -30,29 +34,48 @@ void send_str(const struct device *uart, char *str)
     printk("Device %s sent: \"%s\"\n", uart->name, str);
 }
 
-/* Função para receber uma string via UART */
-void recv_str(const struct device *uart, char *str)
+void print_uart(char *buf)
 {
-    char *head = str;
-    char c;
+	int msg_len = strlen(buf);
 
-    while (!uart_poll_in(uart, &c)) {
-        *head++ = c;
-    }
-    *head = '\0';
+	for (int i = 0; i < msg_len; i++) {
+		uart_poll_out(uart_dev, buf[i]);
+	}
+}
 
-    printk("Device %s received: \"%s\"\n", uart->name, str);
+
+void uart_tx_cb(const struct device *dev, void *user_data)
+{
+	uint8_t c;
+    tx_buff_pos = 0;
+
+	if (!uart_irq_update(uart_dev)) {
+		return;
+	}
+
+	if (!uart_irq_rx_ready(uart_dev)) {
+		return;
+	}
+
+    memset(tx_buff, 0, MSG_LEN);
+
+	
+	while (uart_fifo_read(uart_dev, &c, 1) == 1) {
+		if (c == '\n'  && tx_buff_pos > 0) {
+			tx_buff[tx_buff_pos] = '\0';
+			tx_buff_pos = 0;
+		} else if (tx_buff_pos < (sizeof(tx_buff) - 1)) {
+			tx_buff[tx_buff_pos++] = c;
+		}
+	}
+
+    print_uart("Dados sendo transmitindo:\n");
+    print_uart(tx_buff);
 }
 
 int main(void)
 {
     int rc;
-    char send_buf[64];
-    char recv_buf[64];
-    int i = 10;
-
-    /* Obtém o dispositivo UART pelo nome */
-    //art_dev = device_get_binding(UART_DEVICE_NAME);
 
     /* Verifica se o dispositivo UART está pronto */
     if (!uart_dev) {
@@ -62,39 +85,28 @@ int main(void)
 
     /* Configura o dispositivo UART */
     rc = uart_configure(uart_dev, &uart_cfg);
+
     if (rc) {
         printk("Could not configure device %s\n", uart_dev->name);
     }
+
+    rc = uart_irq_callback_user_data_set(uart_dev, uart_tx_cb, NULL);
+    if (rc < 0) {
+		if (rc == -ENOTSUP) {
+			printk("Interrupt-driven UART API support not enabled\n");
+		} else if (rc == -ENOSYS) {
+			printk("UART device does not support interrupt-driven API\n");
+		} else {
+			printk("Error setting UART callback: %d\n", rc);
+		}
+		return 0;
+	}
+    uart_irq_rx_enable(uart_dev);
+    print_uart("Hello, I'm João\n\n");
 
     /* Loop principal */
-    while (i--) {
-        snprintf(send_buf, 64, "Hello from device %s, num %d", uart_dev->name, i);
-        send_str(uart_dev, send_buf);
-        /* Espera um tempo para a mensagem chegar ao outro dispositivo */
-        k_sleep(K_MSEC(100));
-        recv_str(uart_dev, recv_buf);
-
-        k_sleep(K_MSEC(1000));
-    }
-
-    /* Altera a taxa de transmissão (baudrate) */
-    uart_cfg.baudrate = 9600;
-    printk("\nChanging baudrate of the UART device to %d!\n\n", uart_cfg.baudrate);
-
-    rc = uart_configure(uart_dev, &uart_cfg);
-    if (rc) {
-        printk("Could not configure device %s\n", uart_dev->name);
-    }
-
-    i = 10;
-    while (i--) {
-        snprintf(send_buf, 64, "Hello from device %s, num %d", uart_dev->name, i);
-        send_str(uart_dev, send_buf);
-        /* Espera um tempo para a mensagem chegar ao outro dispositivo */
-        k_sleep(K_MSEC(100));
-        recv_str(uart_dev, recv_buf);
-
-        k_sleep(K_MSEC(1000));
+    while (1) {
+        k_sleep(K_SECONDS(1));
     }
 
     return 0;
