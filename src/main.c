8691d9d983d722bd40cf36493277d3c3f685cbed
JoaoMatheusND <jmnd@ic.ufal.br>
2025-02-17 13:59:23 -0300

    ...

--- src/main.c
+++ src/main.c
@@ -8,9 +8,6 @@
 K_MSGQ_DEFINE(uart_msgq, UART_BUF_SIZE, 10, 4);
 
 #define STACK_SIZE 1024
-K_THREAD_STACK_DEFINE(thread_stack, STACK_SIZE);
-struct k_thread tx_thread_data;
-k_tid_t tx_thread_tid;
 
 //#define UART_DEVICE_NODE DT_NODELABEL(zephyr_shell_uart)  
 #define UART_DEVICE_NODE DT_CHOSEN(zephyr_shell_uart)
@@ -21,21 +18,35 @@ uint8_t tx_buf[UART_BUF_SIZE];
 
 int rx_buf_pos = 0;
 
-void uart_cb(const struct device *dev, void *user_data) {
-    uint8_t c;
+// void uart_cb(const struct device *dev, void *user_data) {
+//     uint8_t c;
     
-    while (uart_fifo_read(dev, &c, 1) > 0) {
-        if (c == '\n') {  
-            rx_buf[rx_buf_pos] = '\0';
-            printk("Recebido: %s\n", rx_buf);
-            rx_buf_pos = 0;  
-            memset(rx_buf, 0, UART_BUF_SIZE);
-        } else {
-            if (rx_buf_pos < UART_BUF_SIZE - 1) {
-                rx_buf[rx_buf_pos++] = c;
-            }
+//     while (uart_fifo_read(dev, &c, 1) > 0) {
+//         if (c == '\n') {  
+//             rx_buf[rx_buf_pos] = '\0';
+//             rx_buf_pos = 0;  
+//             memset(rx_buf, 0, UART_BUF_SIZE);
+//         } else {
+//             if (rx_buf_pos < UART_BUF_SIZE - 1) {
+//                 rx_buf[rx_buf_pos++] = c;
+//             }
+//         }
+//     }
+// }
+
+void rx_task(void *p1, void *p2, void *p3){
+    char rx_buffer[UART_BUF_SIZE] = {0};
+
+    while(true){
+        if(uart_poll_in(uart_dev, rx_buffer) != 0){
+            k_sleep(K_MSEC(100));
+            continue;
         }
+
+        printk("Recebido: %s\n", rx_buffer);
+        
     }
+
 }
 
 void send_by_uart(char *buf)
@@ -57,7 +68,7 @@ void uart_tx_data(const struct device *dev, void *p1, void *p2){
 }
 
 void collect_sensor_data(){
-    char data[UART_BUF_SIZE];
+    char data[UART_BUF_SIZE] = "Temperatura: 25.5°C\n\0";
     while(1){
         // JOTA - Coleta os dados do sensor
 
@@ -73,17 +84,11 @@ void main(void) {
         return;
     }
 
-    // Configura a UART para usar interrupção no RX
-    uart_irq_callback_user_data_set(uart_dev, uart_cb, NULL);
-    uart_irq_rx_enable(uart_dev);
-
-    tx_thread_tid = k_thread_create(&tx_thread_data, thread_stack, STACK_SIZE,
-                                    uart_tx_data, uart_dev, NULL, NULL,
-                                    1, 0, K_NO_WAIT);
-
     while(1){
-        collect_sensor_data();
-        K_
+        k_sleep(K_MSEC(2000));
     }
 
-}
+};
+
+//K_THREAD_DEFINE(tx_task, STACK_SIZE, uart_tx_data, NULL, NULL, NULL, 5, 0, 0);
+K_THREAD_DEFINE(rx_read, STACK_SIZE, rx_task, NULL, NULL, NULL, 5, 0, 0);
